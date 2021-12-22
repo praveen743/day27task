4e2fcf8d70e52c182caa1caa307be28ca8d46e36
praveen743 <praveenrajb2w@gmail.com>
2021-12-22 20:11:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+export default function App() {
+  const [add, setAdd] = useState("");
+  const [details, setDetails] = useState([]);
+
+  const handleAdd = () => {
+    setDetails([...details, add]);
+    setAdd("");
+  };
+
+  const handleDelete = (idx) => {
+    console.log(idx);
+    const deleteElement = details.filter((val, ind) => {
+      return ind !== idx;
+    });
+    setDetails(deleteElement);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='header'>TO DO LIST</h1>
+    <div className='container'>
+    <div className='row'>
+    <div className='col'>
+      
+        <input placeholder='enter the task here' className='input' type="text" value={add} onChange={(e) => setAdd(e.target.value)} />
+        <button className='button' onClick={handleAdd}>Add</button>
+      </div>
+      </div>
+      
+        {details.map((value, id) => {
+          return (
+            <div className='task'
+              key={id}
+              >
+              <p className='para'>{value}</p>
+              <button className='button2' onClick={() => handleDelete(id)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
+     
+     
   );
 }
-
-export default App;
